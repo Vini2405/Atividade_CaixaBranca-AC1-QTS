43391d5553e60b5d1eee6751581202810ca58bde
Vinícius Gibertoni Prado <92763040+Vini2405@users.noreply.github.com>
2023-10-20 12:01:34 -0300

    Update User.java

--- Caixa Branca/login/src/login/User.java
+++ Caixa Branca/login/src/login/User.java
@@ -5,8 +5,14 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+/* Classe User é responsável por estabelecer uma conexão com o banco de dados, 
+ * validar as credenciais do usuário e armazenar o nome do mesmo. 
+ */
 public class User {
 
+	 /* Método conectarBD: Responsável por estabelecer uma conexão com o banco de dados.
+     * @return: retorna a conexão do banco de dados, para que seja possível usá-la em outros métodos. 
+     */
     public Connection conectarBD(){
         Connection conn = null;
         try{
@@ -15,22 +21,44 @@ public class User {
             conn = DriverManager.getConnection(url);
         }catch (Exception e) { }
         return conn;}
+    
+    // As variaveis da classe são definidas e inicializadas
+    // A variavel nome guardará o nome do usuario, caso este consiga fazer login.
     public String nome="";
+    
+    /* A variavel booleana result serve para marcar se o usuário efetuou o login ou não.
+     * Caso o login seja bem-sucedido, o valor será alterado para True.
+     */
     public boolean result = false;
+    
+    /* Método verificarUsuario: Esse método é o responsável por fazer a consulta MYSQL 
+     * com o login e senha informados pelo usuario, para então determinar se o login foi bem-sucedido ou não.
+     * @return: valor booleano que indica se o login foi bem-sucedido ou não.
+     */
     public boolean verificarUsuario(String login, String senha){
         String sql = "";
         Connection conn = conectarBD();
-        //INTRODUÇÃO SQL
+        
+        //INSTRUÇÃO SQL
+        
+        /* Concatenação de String predefinida + login e senha inseridos pelo usuário, 
+         * gerando a linha de SQL que realizará a consulta.
+         */
         sql += "select nome from usuarios ";
         sql +="where login = " + "'" + login + "'";
         sql += " and senha = " + "'" + senha + "';";
         try{
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery(sql);
+            
+            /* O bloco de comando "if" verifica se houve um retorno da consulta realizada anteriormente.
+             * Caso for false, será lançado um erro.
+             * Caso for true, o código continua, e consequentemente, as variaveis "result" e "nome" recebem novos valores.
+             */
             if(rs.next()){
                 result = true;
                 nome = rs.getString("nome");}
             
         }catch (Exception e) { }
         return result; }   
-    }//fim da class
+    } //fim da classe
